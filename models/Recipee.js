6a824d7040ebe81906e24663ef8e12b59577703e
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2021-03-07 18:06:49 +0100

    change branch ok

--- models/Recipee.js
+++ models/Recipee.js
@@ -2,15 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeeSchema = new Schema({
-    name: String,
-    ingredients: String,
-    image: {
-        type: String,
-        default: "https://www.biocoop-cholet.fr/docs/1/Actualites//Quelle-casserole-pour-cuisiner-sain.png"
-    }
-
-})
-
+  name: String,
+  ingredients: String,
+  image: {
+    type: String,
+    default:
+      "https://www.biocoop-cholet.fr/docs/1/Actualites//Quelle-casserole-pour-cuisiner-sain.png",
+  },
+});
 
 const Recipee = mongoose.model("Recipee", recipeeSchema);
 
