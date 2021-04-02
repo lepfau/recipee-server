e7191c1115cfac393b53455d00afd8082b85ffaa
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2021-04-02 10:05:41 +0200

    deployyy

---
+++ public/precache-manifest.5cfdc6cd2e980c8691f0d3329b2daeca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567bbd8488b80db1211a1b8ef63eaf74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13742eeb8083dbf75fb8",
+    "url": "/static/css/main.a401d19b.chunk.css"
+  },
+  {
+    "revision": "0301428f64961238af3c",
+    "url": "/static/js/2.0cb0189a.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.0cb0189a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13742eeb8083dbf75fb8",
+    "url": "/static/js/main.f37ee1c6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "899c61f1d3b1eeca1a6b70eec3eea456",
+    "url": "/static/media/tablecooking.899c61f1.jpg"
+  }
+]);
